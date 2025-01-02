287ade275b45ba8758e009f4d72e571cd35d8f39
liujianzhang.901 <liujianzhang.901@bytedance.com>
2025-01-02 11:29:49 +0800

    添加java目录
    Change-Id: Ib567c82fa2e35513ba7eaccc9f4492dc8034b891

--- app/src/main/kotlin/com/study/recyclerview/activity/SplashActivity.kt
+++ app/src/main/kotlin/com/study/recyclerview/activity/SplashActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import com.study.recyclerview.databinding.ActivitySplashBinding
+import com.study.recyclerview.utils.ViewUtil
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : ComponentActivity() {
@@ -15,7 +16,7 @@ class SplashActivity : ComponentActivity() {
         setContentView(binding.root)
 
         binding.testTv.text = "I am just test!"
-
+        Log.d("TAG", "onCreate: 100dp is " + ViewUtil.dp2px2(this, 100f))
         binding.simpleTest.setOnClickListener {
             Log.d("TAG", "onCreate: simpleTest click!!")
             startActivity(Intent(this, SimpleRecyclerActivity::class.java))
